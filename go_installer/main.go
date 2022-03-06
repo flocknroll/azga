d3e17439eb39b50e265320560f1cf8f067a488af
flocknroll <dosso.florent@gmail.com>
2022-03-06 10:54:20 +0100

    addContent

--- go_installer/main.go
+++ go_installer/main.go
@@ -9,6 +9,8 @@ import (
 	"os"
 )
 
+// Returns a MD5 digest of the lines.
+// Strips the new line character if present.
 func hashLines(lines []string) (int, []byte) {
 	nb := 0
 	md5 := md5.New()
@@ -21,8 +23,9 @@ func hashLines(lines []string) (int, []byte) {
 	return nb, md5.Sum(nil)
 }
 
+// Reads all the lines from a file.
 func readEntireFile(path string) []string {
-	f, err := os.OpenFile(path, os.O_RDONLY, 0444)
+	f, err := os.OpenFile(path, os.O_RDONLY, 0)
 	if err == nil {
 		defer f.Close()
 	} else {
@@ -40,11 +43,12 @@ func readEntireFile(path string) []string {
 	return result
 }
 
+// Iterate through a file and returns the MD5 digests of the lines grouped by the specified number.
 func rollingHashFile(path string, linesNb int) <-chan []byte {
 	ch := make(chan []byte)
 
 	go func() {
-		f, err := os.OpenFile(path, os.O_RDONLY, 0444)
+		f, err := os.OpenFile(path, os.O_RDONLY, 0)
 		if err == nil {
 			defer f.Close()
 		} else {
@@ -74,10 +78,11 @@ func rollingHashFile(path string, linesNb int) <-chan []byte {
 	return ch
 }
 
-func checkContent(src string, dest string) bool {
-	linesNb, srcHash := hashLines(readEntireFile(src))
+// Checks if the source file content if present in the destination file.
+func checkContent(src_path string, dest_path string) bool {
+	linesNb, srcHash := hashLines(readEntireFile(src_path))
 
-	for destHash := range rollingHashFile(dest, linesNb) {
+	for destHash := range rollingHashFile(dest_path, linesNb) {
 		if destHash != nil && bytes.Equal(srcHash, destHash) {
 			return true
 		}
@@ -86,7 +91,34 @@ func checkContent(src string, dest string) bool {
 	return false
 }
 
+// Add the content of the source file at the end of the destination file if not already present.
+func addContent(src_path string, dest_path string) {
+	found := checkContent(src_path, dest_path)
+
+	if found {
+		fmt.Println("Data found")
+	} else {
+		fmt.Println("Data not found - appending")
+		src := readEntireFile(src_path)
+		f, err := os.OpenFile(dest_path, os.O_APPEND|os.O_WRONLY, os.ModeAppend)
+		if err == nil {
+			defer f.Close()
+		} else {
+			panic(err)
+		}
+
+		for _, line := range src {
+			_, err = f.WriteString("\n" + line)
+			if err != nil {
+				panic(err)
+			}
+		}
+	}
+}
+
 func main() {
-	fmt.Println(checkContent("test_data/src_found.txt", "test_data/dest.txt"))
-	fmt.Println(checkContent("test_data/src_not_found.txt", "test_data/dest.txt"))
+	// fmt.Println(checkContent("test_data/src_found.txt", "test_data/dest.txt"))
+	// fmt.Println(checkContent("test_data/src_not_found.txt", "test_data/dest.txt"))
+
+	addContent(os.Args[1], os.Args[2])
 }
